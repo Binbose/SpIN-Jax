2c1518c6d6542742f4b1b102b39c4cf2c06c83eb
Zijian Zhang <doomspec@outlook.com>
2022-03-16 18:04:36 -0400

    Run param_phase_diagram on Slurm

--- param_phase_diagram.py
+++ param_phase_diagram.py
@@ -6,15 +6,12 @@ import sys
 from pathlib import Path
 import json
 
-def save_result(key , value, save_dir="./"):
-    save_file_dir = save_dir+"/param_phase_diagram.json"
-    existing_dict = {}
-    if Path(save_file_dir).exists():
-        with open(save_file_dir,"r") as f:
-            existing_dict = json.load(f)
-    existing_dict[key] = value
+def save_result(moving_average_beta , learning_rate, error_threshold, epoch, save_dir="./param_phase_diagram"):
+    save_file_dir = save_dir+"/{:.3f}_{:.3E}_{:.3f}.json".format(moving_average_beta, learning_rate, error_threshold)
+    if not Path(save_dir).exists():
+        Path(save_dir).mkdir(exist_ok=True)
     with open(save_file_dir,"w") as f:
-        json.dump(existing_dict, f)
+        f.write(str(epoch))
 
 def get_N_epoches_to_converge(moving_average_beta = 1, learning_rate = 1e-5, error_threshold = 0.5):
 
@@ -22,7 +19,7 @@ def get_N_epoches_to_converge(moving_average_beta = 1, learning_rate = 1e-5, err
     trainer.num_epochs = 2000
     trainer.moving_average_beta = moving_average_beta
     trainer.learning_rate = learning_rate
-
+    trainer.save_dir = trainer.save_dir+"_{}_{}".format(moving_average_beta,learning_rate)
     ground_truth = get_exact_eigenvalues(trainer.system,trainer.n_eigenfuncs,trainer.n_space_dimension,trainer.D_min, trainer.D_max, trainer.charge)
 
     result_dict = {"n_epoch_to_threshold": -1}
@@ -31,15 +28,14 @@ def get_N_epoches_to_converge(moving_average_beta = 1, learning_rate = 1e-5, err
         energies = np.asarray(kwargs["energies"])
         newest_energy = np.average(energies[-trainer.window:],axis=0)
         energy_error = abs(np.average(newest_energy - ground_truth))
-        print(moving_average_beta, learning_rate, energy_error)
+        if epoch%20 == 0:
+            print("epoch:{}, energy_error:{}".format(epoch, energy_error))
         if energy_error <= error_threshold:
             result_dict["n_epoch_to_threshold"] = epoch
-            save_result((moving_average_beta,learning_rate,error_threshold),epoch)
+            save_result(moving_average_beta,learning_rate,error_threshold,epoch)
             return True
 
     trainer.start_training(show_progress=False, callback = callback)
-
-    print(result_dict)
 
 if __name__ == "__main__":
     n_points = 5
@@ -47,9 +43,12 @@ if __name__ == "__main__":
     moving_average_beta = [1]
     learning_rate = 1e-5+5e-6*np.linspace(-1,1,n_points)
     param_list = []
-    error_threshold = 1.5
+    error_threshold = 1
     for beta in moving_average_beta:
         for rate in learning_rate:
             param_list.append((beta,rate,error_threshold))
-    for param in param_list:
-        get_N_epoches_to_converge(*param)
+    slurm_array_id = int(sys.argv[1])
+    print(param_list)
+    if(slurm_array_id<=len(param_list)):
+        print(param_list[slurm_array_id-1])
+        get_N_epoches_to_converge(*param_list[slurm_array_id-1])
